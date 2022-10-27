9207952655b41f15a039c9d485533a5f8839cdf4
Jeremy233 <30543532+Jeremy233@users.noreply.github.com>
2022-10-27 02:53:10 -0400

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -17,8 +17,8 @@ class MainPage(object):
         self.page.pack()
         Label(self.page).grid(row=0, stick=W)
 
-        Label(self.page, text='').grid(row=1, stick=W, pady=10) # leave notes
-        Label(self.page, text='').grid(row=1, stick=W, pady=10) # check notes
-        Label(self.page, text='').grid(row=1, stick=W, pady=10) # change password
-        Label(self.page, text='').grid(row=1, stick=W, pady=10) # delete user
-        Label(self.page, text='').grid(row=1, stick=W, pady=10) # log out 
+        Label(self.page, text='查看你的当前留言').grid(row=1, stick=W, pady=10) # leave notes
+        Label(self.page, text='查看他人留言').grid(row=2, stick=W, pady=10) # check notes
+        Label(self.page, text='修改密码').grid(row=3, stick=W, pady=10) # change password
+        Label(self.page, text='删除本用户').grid(row=4, stick=W, pady=10) # delete user
+        Label(self.page, text='登出').grid(row=5, stick=W, pady=10) # log out 

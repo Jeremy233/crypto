2a79c98c1ae2cbcad2820db65fa7df7968430b97
Renzoe-Box <he525@purdue.edu>
2022-10-28 00:55:08 -0400

    page up & page down

--- OthersMessagePage.py
+++ OthersMessagePage.py
@@ -46,56 +46,92 @@ class OthersMessagePage(object):
 	def getMessagesFromOthers(self):
 		others_name = self.others_user_name.get()
 		messages = list(message_collection.find({'user_name': others_name}))
-		for item in messages:
-			print(item['time'])
 		
-		if messages != None:
+		if len(messages) != 0:
 			others_message_window = Toplevel(self.root)
 			others_message_window.geometry('500x400')
 			page_numbers = int(len(messages)/3)
-			print(page_numbers)
 			# current_message = [x * current_page_idx for x in [0,1,2]]
 			current_message = [0, 1, 2]
+
+			def change_text_button(): # message is a list
+				nonlocal messages, m1, m1_button, m2, m2_button, m3, m3_button
+				m1['text'] = messages[self.current_page_idx * 3]['time']
+				m1_button['state'] = 'active'
+				
+				try:
+					m2['text'] = messages[self.current_page_idx * 3 + 1]['time']
+					m2_button['state'] = 'active'
+				except IndexError:
+					m2['text'] = '没有其他留言了'
+					m2_button['state'] = 'disabled'
+				try:
+					m3['text'] = messages[self.current_page_idx * 3 + 2]['time']
+					m3_button['state'] = 'active'
+				except IndexError:
+					m3['text'] = '没有其他留言了'
+					m3_button['state'] = 'disabled'
+				
 
 			def forward():
 				if self.current_page_idx < page_numbers:
 					self.current_page_idx += 1
-					backward_button['state'] = 'enable'
-				else:
-					forward_button['state'] = 'disabled'
+					print('current_page_idx:{0}'.format(self.current_page_idx))
+					if self.current_page_idx == page_numbers:
+						nonlocal forward_button
+						forward_button['state'] = 'disabled'
+					nonlocal m1, m2, m3, backward_button
+					backward_button['state'] = 'active'
+					change_text_button()
+					
+
 
 			def backward():
 				if self.current_page_idx != 0:
 					self.current_page_idx -= 1
-				else:
-					backward_button['state'] = 'disabled'
+					print('current_page_idx:{0}'.format(self.current_page_idx))
+					if self.current_page_idx == 0:
+						nonlocal backward_button
+						backward_button['state'] = 'disabled'
+					nonlocal m1, m2, m3, forward_button
+					forward_button['state'] = 'active'
+					change_text_button()
+
 
 
 			Label(others_message_window, text=others_name+'的留言').grid(row=0, column=1)
-			backward_button = Button(others_message_window, text='<<', command=backward, state=DISABLED).grid(row=2)
-			if page_numbers != 0:
-				forward_button = Button(others_message_window, text='>>', command=forward).grid(row=2, column=4)
-			else:
-				forward_button = Button(others_message_window, text='>>', state=DISABLED).grid(row=2, column=4)
+			backward_button = Button(others_message_window, text='<<', command=backward, state=DISABLED)
+			backward_button.grid(row=2)
+			forward_button = Button(others_message_window, text='>>', command=forward)
+			forward_button.grid(row=2, column=4)
+
+
+			
+			m1 = Label(others_message_window, text=messages[current_message[0]]['time'], bg='white', width=40, height=5)
+			m1.grid(row=1, column=1)
+			m1_button = Button(others_message_window, text='查看')
+			m1_button.grid(row=1, column=2, stick=E)
 
 			try:
-				Label(others_message_window, text=messages[current_message[0]]['time'], bg='blue', width=40, height=5).grid(row=1, column=1)
-				Button(others_message_window, text='查看').grid(row=1, column=2, stick=E)
+				m2 = Label(others_message_window, text=messages[current_message[1]]['time'], bg='white', width=40, height=5)
+				m2.grid(row=2, column=1)
+				m2_button = Button(others_message_window, text='查看')
+				m2_button.grid(row=2, column=2, stick=E)
 			except IndexError:
-				Label(others_message_window, text='没有其他留言了', bg='green', width=40, height=5).grid(row=1, column=1)
-				Button(others_message_window, text='查看', state=DISABLED).grid(row=1, column=2, stick=E)
+				m2 = Label(others_message_window, text='没有其他留言了', bg='white', width=40, height=5)
+				m2.grid(row=2, column=1)
+				m2_button = Button(others_message_window, text='查看', state=DISABLED)
+				m2_button.grid(row=2, column=2, stick=E)
 			try:
-				Label(others_message_window, text=messages[current_message[1]]['time'], bg='blue', width=40, height=5).grid(row=2, column=1)
-				Button(others_message_window, text='查看').grid(row=2, column=2, stick=E)
-			except IndexError:
-				Label(others_message_window, text='没有其他留言了', bg='green', width=40, height=5).grid(row=2, column=1)
-				Button(others_message_window, text='查看', state=DISABLED).grid(row=2, column=2, stick=E)
-			try:
-				Label(others_message_window, text=messages[current_message[2]]['time'], bg='blue', width=40, height=5).grid(row=3, column=1)
-				Button(others_message_window, text='查看').grid(row=3, column=2, stick=E)
+				m3 = Label(others_message_window, text=messages[current_message[2]]['time'], bg='white', width=40, height=5)
+				m3.grid(row=3, column=1)
+				m3_button = Button(others_message_window, text='查看')
+				m3_button.grid(row=3, column=2, stick=E)
 			except:
-				Label(others_message_window, text='没有其他留言了', bg='green', width=40, height=5).grid(row=3, column=1)
-				Button(others_message_window, text='查看', state=DISABLED).grid(row=3, column=2, stick=E)
+				m3 = Label(others_message_window, text='没有其他留言了', bg='white', width=40, height=5)
+				m3.grid(row=3, column=1)
+				m3_button = Button(others_message_window, text='查看', state=DISABLED)
+				m3_button.grid(row=3, column=2, stick=E)
 
 
 
